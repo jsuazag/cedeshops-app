00feeca6036d297c48e860384f5165aa6e59b074
Jerson Suaza <jerson.suaza@gmail.com>
2022-09-17 12:56:45 -0500

    new pages

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import { Page } from "../../components/Page";
 
 export const Login = () => (
-    <Page>
-        <h1>Login Page</h1>
-    </Page>
-)
+  <Page>
+    <h1>Login Page</h1>
+  </Page>
+);
