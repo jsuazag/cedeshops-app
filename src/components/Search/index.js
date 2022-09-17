453c476a6cb1506e527a755012ce36b24e32a339
Jerson Suaza <jerson.suaza@gmail.com>
2022-09-17 11:30:03 -0500

    fix box-sizing

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,32 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { SearchWrapper, SearchInput, SearchClearWrapper } from "./styles";
 import { IoClose } from "react-icons/io5";
 
 export const Search = ({ searchTerm = "" }) => {
-
   const [searchText, setSearchText] = useState(searchTerm);
 
   const handlerChangeText = (event) => {
     setSearchText(event.target.value);
-  }
+  };
 
   const clearSearchHandler = () => {
-    setSearchText('');
-  }
+    setSearchText("");
+  };
 
   return (
     <SearchWrapper>
-      <SearchInput 
-        value={searchText} 
-        onChange={handlerChangeText} 
-        type="text" 
+      <SearchInput
+        value={searchText}
+        onChange={handlerChangeText}
+        type="text"
       />
-      <SearchClearWrapper 
-        onClick={clearSearchHandler} 
+      <SearchClearWrapper
+        onClick={clearSearchHandler}
         hasText={searchText !== ""}
       >
         <IoClose />
       </SearchClearWrapper>
     </SearchWrapper>
   );
-}
+};
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+
+    *, ::after, ::before {
+        box-sizing: border-box;
+    }
+
     body {
         margin: 0;
         padding: 0;
