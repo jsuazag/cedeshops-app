5ef4f23209a8e6d2122d1b4b6fa9de2ce6b4d335
Jerson Suaza <jerson.suaza@gmail.com>
2022-09-24 13:01:54 -0500

    form styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,14 +1,61 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import { Theme } from "./themes/theme";
 
 export const GlobalStyle = createGlobalStyle`
-
     *, ::after, ::before {
         box-sizing: border-box;
     }
-
     body {
         margin: 0;
         padding: 0;
         font-family: "Lato";
     }
+`;
+
+export const FormControl = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 6px 0;
+`;
+
+export const FormControLabel = styled.label`
+  font-size: 1.05em;
+  color: ${Theme.primary_text};
+`;
+
+export const FormControlInput = styled.input`
+  font-family: "Lato";
+  font-size: 1.1em;
+  color: ${Theme.primary_text};
+  padding: 6px 8px;
+  width: 100%;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  outline: none;
+`;
+
+export const FormControlAction = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 8px 0;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  border-radius: 6px;
+  padding: 10px 8px;
+  font-family: "Lato";
+  color: ${ props => props.textColor || "#fff" };
+  background-color: ${ props => props.bgColor || Theme.primary };
+  border-color: ${ props => props.borderColor };
+  border-style: ${ props => props.borderColor ? "solid" : "none" } ;
+  text-align: center;
+  font-weight: 700;
+  cursor: pointer;
+  font-size: 0.9em;
+  opacity: 0.9;
+  &:hover {
+    opacity: 1;
+  }
 `;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,29 @@
 import { Page } from "../../components/Page";
+import {
+  Button,
+  FormControl,
+  FormControLabel,
+  FormControlAction,
+  FormControlInput,
+} from "../../globalStyles";
 
 export const Login = () => (
   <Page>
-    <h1>Login Page</h1>
+    <h1>Iniciar Sesión</h1>
+    <form>
+      <FormControl>
+        <FormControLabel>Correo electrónico</FormControLabel>
+        <FormControlInput type="email" />
+      </FormControl>
+
+      <FormControl>
+        <FormControLabel>Contraseña</FormControLabel>
+        <FormControlInput type="password" />
+      </FormControl>
+
+      <FormControlAction>
+        <Button>Ingresar</Button>
+      </FormControlAction>
+    </form>
   </Page>
 );
